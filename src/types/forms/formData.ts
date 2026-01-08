e5027e90f381031ef88d98dacc0bfba08d148db6
SanketTheCodeCrafter <sanket.nagap3@gmail.com>
2026-01-08 20:03:21 +0530

    feat(forms): add FormData types for dynamic form handling

---
+++ src/types/forms/formData.ts
@@ -0,0 +1,22 @@
+import type { FormField } from "./formField";
+import type { TextField } from "./textField";
+import type { NumberField } from "./numberField";
+import type { SelectField } from "./selectField";
+import type { FormSchema } from "./formSchema";
+
+export type FieldValue<F extends FormField> =
+  F extends TextField ? string :
+  F extends NumberField ? number :
+  F extends SelectField ? string :
+  never;
+
+
+  export type FormDataFromFields<
+  Fields extends readonly FormField[]
+  > = {
+    [F in Fields[number] as F['id']]: FieldValue<F>
+  }
+
+export type FormDataFromSchema<
+Schema extends FormSchema
+> = FormDataFromFields<Schema['fields']>;
